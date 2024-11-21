6fb55944093c6ee4ab3b117b49afcde3cccbe536
Emran-Y <emranyonas602@gmail.com>
2024-11-22 00:47:33 +0300

    removed auth

--- server/index.js
+++ server/index.js
@@ -70,17 +70,6 @@ const Chat = mongoose.model('Chat', chatSchema);
 
 
 
-const authenticate = (req, res, next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Invalid token' });
-    req.user = user;
-    next();
-  });
-};
-
 // Routes
 
 // Login route
